08a9af644929c1902c418ad18990f8bc637be9ce
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-18 16:19:30 +0600

    count answers and data class word are added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,23 @@
 import java.io.File
 
 fun main() {
-
+    val listOfWord = mutableListOf<Word>()
     val wordsFile: File = File("dictionary.txt")
     val words = wordsFile.readLines()
-    for (element in words) {
-        println(element)
+    for (string in words) {
+        val splitedStringWords = string.split(" ")
+        val splitedStringAnswer = string.split("|")
+        listOfWord.add(Word(splitedStringWords[0], splitedStringWords[1], splitedStringAnswer[1].toInt() ?: 0))
+    }
+    listOfWord.forEach { println(it) }
+}
+
+data class Word(
+    private val englishWord: String,
+    private val russianWord: String,
+    private val correctAnswersCount: Int?,
+) {
+    override fun toString(): String {
+        return "Word(englishWord='$englishWord', russianWord='$russianWord', correctAnswersCount=$correctAnswersCount)"
     }
 }
